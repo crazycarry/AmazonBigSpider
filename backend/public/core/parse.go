09912ebd18de10769ce4e82a5479357149b710be
pipik <569929309@qq.com>
2019-01-29 11:53:29 +0800

    bug fix util

--- backend/public/core/parse.go
+++ backend/public/core/parse.go
@@ -260,7 +260,7 @@ func ParseDetail(url string, content []byte) map[string]string {
 		rank = ranktemp
 	}
 	returnmap["rank"] = util.IS(rank)
-	returnmap["createtime"] = util.GetSecord2DateTimes(util.GetSecordTimes())
+	returnmap["createtime"] = util.GetSecond2DateTimes(util.GetSecondTimes())
 
 	//purl
 	purl := ""
